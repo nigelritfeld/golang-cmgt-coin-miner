e04579b127187f7fee7fca4389d10753a94f4dcb
nigelritfeld <nigelritfeld@live.nl>
2023-03-06 11:39:21 +0100

    Added hash unit test

--- main_test.go
+++ main_test.go
@@ -59,3 +59,34 @@ func TestHashSHA256(t *testing.T) {
 		})
 	}
 }
+
+func TestHash(t *testing.T) {
+	type args struct {
+		phrase string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "Test example hash",
+			args: args{"000078454c038871fa4d67b0022a30baaf25eaa231f8991b108e2624f052f3f8CMGT Mining CorporationBob PIKAB11548689513858154874778871610312"},
+			want: "00005d430ce77ad654b5309a770350bfb4cf49171c682330a2eccc98fd8853cf",
+		},
+		{
+			name: "Test second hash",
+			args: args{"0000a41723a9797cd18a8c8f462fbd04ae1bcc010c814704161e570c5a6791acCMGT Mining CorporationNigel 1004416116768966634841676908077111116100"},
+			want: "00000e04de1e13322b500c5ac442a2f6f99454aeac164bc52af0be17774e3c96",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := mod10.HashPayload(tt.args.phrase)
+			fmt.Println("GENERATED HASH", got)
+			if got != tt.want {
+				t.Errorf("TryNonce() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
